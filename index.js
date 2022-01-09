b64e49ad9d007c632e5e110cb14f7ba0edfe7aa1
Eric Harrison <github@hrrsn.co>
2022-01-08 20:19:26 -0500

    Test new google method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-import { google } from 'googleapis';
+const queryString = require('query-string');
 
 const googleConfig = {
 	clientId: process.env.GOOGLE_CLIENT_ID,
@@ -18,56 +18,50 @@ app.set('view engine', 'ejs');
  * Create the google auth object which gives us access to talk to google's apis.
  */
 function createConnection() {
-	return new google.auth.OAuth2(
-		googleConfig.clientId,
-		googleConfig.clientSecret,
-		googleConfig.redirect
-	);
+	// return new google.auth.OAuth2(
+	// 	googleConfig.clientId,
+	// 	googleConfig.clientSecret,
+	// 	googleConfig.redirect
+	// );
 }
-
-/**
- * This scope tells google what information we want to request.
- */
-const defaultScope = [
-	'https://www.googleapis.com/auth/plus.me',
-	'https://www.googleapis.com/auth/userinfo.email',
-];
 
 /**
  * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
  */
 function getConnectionUrl(auth) {
-	return auth.generateAuthUrl({
-		access_type: 'offline',
-		prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
-		scope: defaultScope
-	});
+	// return auth.generateAuthUrl({
+	// 	access_type: 'offline',
+	// 	prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
+	// 	scope: defaultScope
+	// });
 }
 
 /**
  * Create the google url to be sent to the client.
  */
 function urlGoogle() {
-	const auth = createConnection(); // this is from previous step
-	const url = getConnectionUrl(auth);
-	return url;
+	const stringifiedParams = queryString.stringify({
+		client_id: process.env.GOOGLE_CLIENT_ID,
+		redirect_uri: 'https://fitwizard.herokuapp.com/authenticate/google',
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.email',
+			'https://www.googleapis.com/auth/userinfo.profile',
+		].join(' '), // space separated string
+		response_type: 'code',
+		access_type: 'offline',
+		prompt: 'consent',
+	});
+
+	const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
+	return googleLoginUrl;
 }
 
-function getGoogleAccountFromCode(code) {
-	const data = await auth.getToken(code);
-	const tokens = data.tokens;
-	const auth = createConnection();
-	auth.setCredentials(tokens);
-	const plus = getGooglePlusApi(auth);
-	const me = await plus.people.get({ userId: 'me' });
-	const userGoogleId = me.data.id;
-	const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
-	return {
-		id: userGoogleId,
-		email: userGoogleEmail,
-		tokens: tokens,
-	};
-}
+app.get('/authenticate/google', (req, res) => {
+	const urlParams = queryString.parse(req.query.code);
+	console.log(urlParams);
+
+	res.json(urlParams);
+});
 
 app.get('/login', (req, res) => {
 	let url = urlGoogle();
