f7bd02b0163c68c2eec70d192c34a186fef0596b
Eric Harrison <github@hrrsn.co>
2022-01-08 19:34:25 -0500

    Add button to manually test

--- public/js/poll.js
+++ public/js/poll.js
@@ -16,3 +16,11 @@ setInterval(function() {
 	xhr.send();
 
 }, 5000);
+
+document.getElementById('buzz').addEventListener('click', function() {
+	document.body.style.backgroundColor = 'blue';
+	navigator.vibrate(300);
+	setTimeout(function() {
+		document.body.style.backgroundColor = 'mediumseagreen';
+	},400);
+});
